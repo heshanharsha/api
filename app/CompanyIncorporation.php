5f29f3a4cdd582cc95d102008657529854358a03
Heshan Harsha <heshan039@gmail.com>
2018-09-17 11:28:10 +0530

    Initial commit

---
+++ app/CompanyIncorporation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyIncorporation extends Model
+{
+    protected $table = 'eroc_companies';
+    protected $primaryKey = "id";
+}
